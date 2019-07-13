199ba2c180b47a8f2cf3da412b11e2f535808b8c
sangcomz <sangcomz@daum.net>
2019-07-13 22:55:03 +0900

    init judge

---
+++ src/Day1.kt
@@ -0,0 +1,35 @@
+class Day1 {
+    fun evenNumbers(array: List<Int>, number: Int): List<Int> {
+        // good luck
+        return array.filter { it % 2 == 0 }.takeLast(number)
+    }
+
+    fun getCount(str: String): Int {
+        return str.filter {
+            it == 'a'
+                    || it == 'e'
+                    || it == 'i'
+                    || it == 'o'
+                    || it == 'u'
+        }.length
+    }
+
+    /**
+     * pp0 현재 인구
+     * percent 1 year
+     * aug 이주민
+     * p 목표 인구
+     */
+    fun nbYear(pp0: Int, percent: Double, aug: Int, p: Int): Int {
+        var currentPopulation = pp0
+        var year = 0
+        while (currentPopulation < p) {
+            year++
+            currentPopulation += (currentPopulation * percent / 100).toInt() + aug
+        }
+        return year
+    }
+    //best
+//    fun nbYear(pp0: Int, percent: Double, aug: Int, p: Int): Int =
+//        generateSequence(pp0.toDouble()) { it * (1 + percent / 100) + aug }.takeWhile { it < p }.count()
+}
---
+++ src/Day2.kt
@@ -0,0 +1,18 @@
+import java.math.BigDecimal
+
+class Day2 {
+    fun highAndLow(numbers: String): String {
+        val list = numbers.split(" ").map { it.toInt() }
+        return "${list.max()} ${list.min()}"
+    }
+
+    //fun highAndLow(numbers: String) = numbers.split(' ').map(String::toInt).let { "${it.max()} ${it.min()}" }
+
+
+    fun tribonacci(signature: DoubleArray, n: Int) = signature.copyOf(n)
+        .also {
+            for (i in 3 until n) {
+                it[i] = (it[i - 1].toBigDecimal() + it[i - 2].toBigDecimal() + it[i - 3].toBigDecimal()).toDouble()
+            }
+        }
+}
